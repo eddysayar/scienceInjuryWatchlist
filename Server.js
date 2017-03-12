75f07b39761d07a686fb2888b3c5af70694ec507
safe admin <safeAdmin@ubuntu-512mb-sfo1-01.localdomain>
2017-03-12 04:40:51 +0000

    server changes

--- Server.js
+++ Server.js
@@ -4,8 +4,8 @@ var express = require("express"),
 
 var app = express();
 
-app.listen(3000, function() {
-    console.log("Live at Port 3000");
+app.listen(80, function() {
+    console.log("Live at Port 80");
 });
 
 app.use(bodyParser.json());
